d4f8bc22b443b13959bf1cad3264947e5ea7c7b5
Piyush-jangid-01 <jangidpiyush16011@gmail.com>
2026-07-28 21:16:50 +0530

    day-8 commit

--- client/src/components/TaskCard.tsx
+++ client/src/components/TaskCard.tsx
@@ -49,11 +49,6 @@ export default function TaskCard({ task, onClick, onDragStart }: TaskCardProps) 
   return (
     <article
       draggable
-      onDragOver={(e) => {
-        e.preventDefault();
-        setIsOver(true);
-      }}
-      onDragLeave={() => setIsOver(false)}
       onDragStart={() => onDragStart(task)}
       onClick={() => onClick(task)}
       className="group flex h-full flex-col rounded-2xl border border-zinc-200 bg-white cursor-pointer p-5 shadow-sm transition-all duration-300 hover:-translate-y-1 hover:border-violet-300 hover:shadow-xl"
